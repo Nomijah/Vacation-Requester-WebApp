652846642b243940c8e6f6d67cff56afbd51bf45
Charlie Skibsted <charlieskibsted@gmail.com>
2023-10-19 13:45:39 +0200

    addeded .env files

--- src/apicalls/userLogin.ts
+++ src/apicalls/userLogin.ts
@@ -1,20 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
-const userLogin = (loginCredentials : {email:string, password:string}) => {
-    return new Promise((resolve, reject) => {
-    axios.post(`https://localhost:7016/Authentication/Login`, 
-    JSON.stringify(loginCredentials), 
-    {headers:{"Content-Type": "application/json",},
-    withCredentials : true,})
-    .then((res) => {
+const userLogin = (loginCredentials: {
+  email: string;
+  password: string;
+}) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(
+        `${import.meta.env.VITE_API_URL}/Authentication/Login`,
+        JSON.stringify(loginCredentials),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
         console.log(res);
         resolve(res);
-    })
-    .catch((err) => {
-            console.log(err);
-            reject(err);
-        })
-    })
-}
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
 
-    export default userLogin;
+export default userLogin;
