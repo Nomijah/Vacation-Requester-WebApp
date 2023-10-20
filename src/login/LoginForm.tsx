81747473d663ae3e7b820869182f3898f1301b40
PetterBostrom <petter.bostrom@gmail.com>
2023-10-20 09:38:33 +0200

    Staff view basics done

--- src/login/LoginForm.tsx
+++ src/login/LoginForm.tsx
@@ -8,39 +8,41 @@ function LoginForm({
   formData: { email: string; password: string };
 }) {
   return (
-    <div className="border rounded bg-light bg-opacity-50">
-      <form onSubmit={handleSubmit}>
-        <div className="form-group m-3">
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            name="email"
-            id="email"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-            value={formData.email}
-            onChange={handleChange}
-            autoComplete="email"
-          />
-        </div>
-        <div className="form-group m-3">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            name="password"
-            id="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-            autoComplete="off"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary m-3 mt-1">
-          Login
-        </button>
-      </form>
+    <div className="loginBody">
+      <div className="border rounded bg-light bg-opacity-50">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group m-3">
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              id="email"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              value={formData.email}
+              onChange={handleChange}
+              autoComplete="email"
+            />
+          </div>
+          <div className="form-group m-3">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              id="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              autoComplete="off"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary m-3 mt-1">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/staffview/Containers/ApplyViewContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ApplyViewContainer() {
+  return (
+    <div>
+      <h1>Test från apply</h1>
+    </div>
+  );
+}
+
+export default ApplyViewContainer;
