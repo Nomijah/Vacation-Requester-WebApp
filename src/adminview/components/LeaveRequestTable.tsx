d38c3a2a76ff70a1b21742c7cf19ccd636625db1
Alvin strandberg <Str4ndb3rg@live.se>
2023-10-20 11:54:16 +0200

    fixed maps

--- src/adminview/components/LeaveRequestTable.tsx
+++ src/adminview/components/LeaveRequestTable.tsx
@@ -15,7 +15,6 @@ const LeaveRequestTable: React.FC<LeaveRequestTableProps> = ({
         <thead>
           <tr>
             <th>Name</th>
-
             <th>Start Date</th>
             <th>End Date</th>
             <th>Date Requested</th>
@@ -26,10 +25,18 @@ const LeaveRequestTable: React.FC<LeaveRequestTableProps> = ({
           {leaveRequests.map((leaveRequest, index) => (
             <tr key={index}>
               <td>{leaveRequest.employeeName}</td>
-              <td>{leaveRequest.startDate}</td>
-              <td>{leaveRequest.endDate}</td>
-              <td>{leaveRequest.dateRequested}</td>
-              <td>{leaveRequest.approvalState}</td>
+              <td>{new Date(leaveRequest.startDate).toLocaleDateString()}</td>
+              <td>{new Date(leaveRequest.endDate).toLocaleDateString()}</td>
+              <td>
+                {new Date(leaveRequest.dateRequested).toLocaleDateString()}
+              </td>
+              <td>
+                {if({leaveRequest.approvalState} === 1){"Pending"}
+                else if({leaveRequest.approvalState} === 2)
+                {"Approved"} else if({leaveRequest.approvalState} === 3)
+                {"Rejected"} else
+                {}}
+              </td>
             </tr>
           ))}
         </tbody>
