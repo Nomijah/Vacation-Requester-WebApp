83c1a03d2a289ebdeb76533969ac4e5325b98a04
Alvin strandberg <Str4ndb3rg@live.se>
2023-10-25 15:29:36 +0200

    fixed button css in LeavetypeDelete

--- src/adminview/components/DeleteLeaveTypeForm.tsx
+++ src/adminview/components/DeleteLeaveTypeForm.tsx
@@ -14,35 +14,41 @@ function DeleteLeaveTypeForm({ selectedLeaveType, handleDelete }) {
       <div className="collapse" id="collapseDelete">
         <div className="card card-body">
           <form action="" className="my-3">
-            <div className="input-group d-flex justify-content-center">
+            <div className="input-group d-flex justify-content-center flex-column row">
               {/* Set the value of the input to the current formData, and call handleChange when the input value changes */}
-              <span className="fw-bold">Do you want to delete&nbsp;</span>
-              <span className="text-danger fw-bold">
+              <p>
                 {" "}
-                {selectedLeaveType.type}{" "}
-              </span>
-              <span className="fw-bold">?</span>
-              <button
-                className="btn btn-success me-3 rounded"
-                type="submit"
-                onClick={handleDelete}
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseDelete"
-                aria-expanded="false"
-                aria-controls="collapseDelete"
-              >
-                Delete
-              </button>
-              <button
-                className="btn btn-danger ms-3 rounded w-25"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseDelete"
-                aria-expanded="false"
-                aria-controls="collapseDelete"
-              >
-                No
-              </button>
+                <span className="fw-bold">Do you want to delete&nbsp;</span>
+                <span className="text-danger fw-bold">
+                  {" "}
+                  {selectedLeaveType.type}{" "}
+                </span>
+                <span className="fw-bold">?</span>{" "}
+              </p>
+              <div>
+                {" "}
+                <button
+                  className="btn btn-success me-3 rounded w-25"
+                  type="submit"
+                  onClick={handleDelete}
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseDelete"
+                  aria-expanded="false"
+                  aria-controls="collapseDelete"
+                >
+                  Delete
+                </button>
+                <button
+                  className="btn btn-danger ms-3 rounded w-25"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseDelete"
+                  aria-expanded="false"
+                  aria-controls="collapseDelete"
+                >
+                  No
+                </button>
+              </div>
             </div>
           </form>
         </div>
