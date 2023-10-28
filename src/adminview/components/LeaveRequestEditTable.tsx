8febfb51bd9c022c8cabe3fae24fb867b9e21d9c
Alvin strandberg <Str4ndb3rg@live.se>
2023-10-28 13:02:47 +0200

    fixed admin list

--- src/adminview/components/LeaveRequestEditTable.tsx
+++ src/adminview/components/LeaveRequestEditTable.tsx
@@ -74,28 +74,47 @@ const LeaveRequestEditTable: React.FC<LeaveRequestTableProps> = ({
   }
 
   return (
-    <div>
+    <div className="container-fluid bg-light-subtle border border-dark-subtle rounded mt-3">
       <table className="table mt-4">
         <thead>
           <tr className="border-1">
-            <th onClick={() => requestSort(`employeeName`)}>
+            <th
+              className="bg-dark-subtle"
+              onClick={() => requestSort(`employeeName`)}
+            >
               Name <ArrowDownUp color="royalblue" size={13} />
             </th>
-            <th onClick={() => requestSort("startDate")}>
+            <th
+              className="bg-dark-subtle"
+              onClick={() => requestSort("startDate")}
+            >
               Start Date <ArrowDownUp color="royalblue" size={13} />
             </th>
-            <th onClick={() => requestSort("endDate")}>
+            <th
+              className="bg-dark-subtle"
+              onClick={() => requestSort("endDate")}
+            >
               End Date <ArrowDownUp color="royalblue" size={13} />
             </th>
-            <th onClick={() => requestSort("dateRequested")}>
+            <th
+              className="bg-dark-subtle"
+              onClick={() => requestSort("dateRequested")}
+            >
               Date Requested <ArrowDownUp color="royalblue" size={13} />
             </th>
-            <th onClick={() => requestSort("leaveType")}>
+            <th
+              className="bg-dark-subtle"
+              onClick={() => requestSort("leaveType")}
+            >
               Leave Type <ArrowDownUp color="royalblue" size={13} />
             </th>
-            <th onClick={() => requestSort("approvalState")}>
+            <th
+              className="bg-dark-subtle"
+              onClick={() => requestSort("approvalState")}
+            >
               Approval State <ArrowDownUp color="royalblue" size={13} />
             </th>
+            <th className="bg-dark-subtle"></th>
           </tr>
         </thead>
         <tbody>
@@ -112,15 +131,21 @@ const LeaveRequestEditTable: React.FC<LeaveRequestTableProps> = ({
                 formState={formState}
               />
             ) : (
-              <tr className="grow-0 mw-100" key={index}>
-                <td>{leaveRequest.employeeName}</td>
-                <td>{new Date(leaveRequest.startDate).toLocaleDateString()}</td>
-                <td>{new Date(leaveRequest.endDate).toLocaleDateString()}</td>
-                <td>
+              <tr className="grow-0 mw-100 align-middle" key={index}>
+                <td className="bg-light-subtle">{leaveRequest.employeeName}</td>
+                <td className="bg-light-subtle">
+                  {new Date(leaveRequest.startDate).toLocaleDateString()}
+                </td>
+                <td className="bg-light-subtle">
+                  {new Date(leaveRequest.endDate).toLocaleDateString()}
+                </td>
+                <td className="bg-light-subtle">
                   {new Date(leaveRequest.dateRequested).toLocaleDateString()}
                 </td>
-                <td>{leaveRequest.leaveType}</td>
-                <td>{renderApprovalState(leaveRequest.approvalState)}</td>
+                <td className="bg-light-subtle">{leaveRequest.leaveType}</td>
+                <td className="bg-light-subtle">
+                  {renderApprovalState(leaveRequest.approvalState)}
+                </td>
 
                 <td>
                   <button
